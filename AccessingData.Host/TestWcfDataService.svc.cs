5fd6bcee6d47b096c9e68940d05bac2f45495b19
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-07-11 18:59:15 +0300

    Add WCF Data Service

---
+++ AccessingData.Host/TestWcfDataService.svc.cs
@@ -0,0 +1,33 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Data.Services.Providers;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace AccessingData.Host
+{
+    public class TestWcfDataService : EntityFrameworkDataService<TestEntities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            config.SetEntitySetAccessRule("Accounts", EntitySetRights.AllRead);
+            config.SetEntitySetAccessRule("Customers", EntitySetRights.AllRead);
+            config.SetEntitySetAccessRule("Transactions", EntitySetRights.AllRead);
+            config.SetEntitySetAccessRule("TransactionDetails", EntitySetRights.AllRead);
+
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+        }
+    }
+}
